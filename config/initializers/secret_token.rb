58ed3eb5ec06c58b9528efe06fd58168c60e3cde
Steve Long <steven989@gmail.com>
2014-04-16 21:03:25 -0400

    initial commit: set up project, created pages#index, and set color scheme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Givr::Application.config.secret_key_base = '246f6494a1fef3e0a93327f7b789469cffd1299862128e93e05a6dcd79ea10aea030bb54697534b46c8732211302d209bf24cab0a09be2e5a2c0c06dcb064e9f'
