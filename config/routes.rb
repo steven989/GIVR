bb43b31d6bad36d9d1b34ed7e228a3ac9e193618
Steve Long <steven989@gmail.com>
2014-04-20 18:29:36 -0400

    created projects page skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Givr::Application.routes.draw do
 
   root to: 'pages#index'
 
+  resources :projects, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
