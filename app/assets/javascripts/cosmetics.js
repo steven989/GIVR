73398e426008858f94d5a95d83bbc35f58e1c25d
Steve Long <steven989@gmail.com>
2014-04-28 15:58:43 -0400

    using the fade effect instead of slide effect

--- app/assets/javascripts/cosmetics.js
+++ app/assets/javascripts/cosmetics.js
@@ -23,16 +23,23 @@ $('document').ready(function() {
 
 function scrollCheck() {
 
+    var scrollPosition = window.scrollY
 
-    var scrollPosition = window.scrollY;
+    var threshold = $('#who_are_we').outerHeight(false) // the threshold is set to the height of the #who_are_we div because when we scroll past the bottom of this div we want to trigger the header shrinking
 
-    var threshold = $('#who_are_we').outerHeight(false); // the threshold is set to the height of the #who_are_we div because when we scroll past the bottom of this div we want to trigger the header shrinking
+    var transitionLength = 200;
 
-    var heightToSubtract = Math.max(0,Math.min(scrollPosition - threshold,headerHeight)); // the height to subtract is never less than 0 or more than the original height of the header div
-
-    $('#visible_blue').css({height: headerHeight - heightToSubtract });
-
+    if (scrollPosition > threshold) { 
+        // $('#visible_white').fadeIn(); 
+        $('#visible_blue').fadeOut(transitionLength);
+    }
+    else 
+        {  
+            // $('#visible_white').fadeOut(transitionLength);
+            $('#visible_blue').fadeIn(); 
+    }    
 }
+
 
 // this code is to make the underline below the navigation bar stay if the user is currently on the page
 
