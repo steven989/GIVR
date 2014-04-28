13dad5ecf30bc6773883e925adc1629e4e597768
Steve Long <steven989@gmail.com>
2014-04-27 22:06:28 -0400

    completed the slide up effect to transition from blue to white header

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,28 +1,24 @@
 
+var headerHeight
 
-$(window).ready(
-
+$('document').ready(function() {
+    
     $(document).on('scroll', scrollCheck)
+    headerHeight = $('#visible_blue').outerHeight(false)
 
-    );
+    });
 
 
 function scrollCheck() {
+
 
     var scrollPosition = window.scrollY
 
     var threshold = $('#who_are_we').outerHeight(false) // the threshold is set to the height of the #who_are_we div because when we scroll past the bottom of this div we want to trigger the header shrinking
 
-    var transitionLength = 200;
+    var heightToSubtract = Math.max(0,Math.min(scrollPosition - threshold,headerHeight))
 
-    if (scrollPosition > threshold) { 
-        // $('#visible_white').fadeIn(); 
-        $('#visible_blue').fadeOut(transitionLength);
-    }
-    else 
-        {  
-            // $('#visible_white').fadeOut(transitionLength);
-            $('#visible_blue').fadeIn(); 
-    }    
+    $('#visible_blue').css({height: headerHeight - heightToSubtract })
+
 }
 
