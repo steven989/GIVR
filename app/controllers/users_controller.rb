78c68edbe1f90d707ff000b1fd996a584725d463
Steve Long <steven989@gmail.com>
2014-05-12 15:16:38 -0400

    user sign up and login using sorcery

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end 
+
+    def create
+
+        @user = User.new(users_params)
+
+        if @user.save
+            auto_login(@user)
+            redirect_to projects_path
+        else 
+            render :new
+        end 
+
+    end 
+
+    private
+
+    def users_params
+        params.require(:user).permit(:email,:password,:password_confirmation)
+    end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Givr::Application.routes.draw do
 
   resources :projects, only: [:index]
 
+  resources :users, only: [:show, :new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
