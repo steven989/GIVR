ec94517d18fb09a55ff077b762b3e8c45d285174
zap-rowsdower <brock.whitbread@gmail.com>
2014-05-12 14:36:45 -0400

    Fill out projects controller and start adding ability to interact with projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,43 @@
 class ProjectsController < ApplicationController
 
-    def index
+def index
+    # @projects = Projects.all
+  end
 
-    end 
+  def show
+    @project = Project.find(params[:id])
+  end
 
+  def new
+    @project = Project.new
+  end
 
+  def create
+    @project = Project.new(projects_params)
+
+    if @projects.save
+      redirect_to projects_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+  def projects_params
+    params.require(:project).permit(:name, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Givr::Application.routes.draw do
 
   root to: 'pages#index'
 
-  resources :projects, only: [:index]
+  resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
