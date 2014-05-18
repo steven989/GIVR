feff99316f24dc8c69004c79545107bfc79784ab
zap-rowsdower <brock.whitbread@gmail.com>
2014-05-18 14:50:15 -0400

    Implement LinkedIn sign up.

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -84,13 +84,12 @@ Rails.application.config.sorcery.configure do |config|
   # - user info fields go to https://developer.linkedin.com/documents/profile-fields
   # - access permissions go to https://developer.linkedin.com/documents/authentication#granting
   #
-  # config.linkedin.key = ""
-  # config.linkedin.secret = ""
-  # config.linkedin.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=linkedin"
-  # config.linkedin.user_info_fields = ['first-name', 'last-name']
-  # config.linkedin.user_info_mapping = {first_name: "firstName", last_name: "lastName"}
-  # config.linkedin.access_permissions = ['r_basicprofile']
-  #
+  config.linkedin.key = Figaro.env.linkedin_app_id
+  config.linkedin.secret = Figaro.env.linkedin_secret_key
+  config.linkedin.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=linkedin"
+  config.linkedin.user_info_fields = ['email']
+  config.linkedin.user_info_mapping = {:email => "email"}
+  config.linkedin.access_permissions = ['r_basicprofile', 'r_emailaddress']
   #
   # For information about XING API:
   # - user info fields go to https://dev.xing.com/docs/get/users/me
