16cbd52727d70e92893277850a7928e9c42f4029
Steve Long <steven989@gmail.com>
2014-05-20 23:16:10 -0400

    only showing active projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,15 +12,15 @@ class ProjectsController < ApplicationController
         filter_id = filter['filterid']
       filter_conditions[filter_type] << filter_id
     end
-      @projects = Project.where("category_id in (?) AND cause_id in (?) AND location_id in (?)", filter_conditions[:category], filter_conditions[:cause], filter_conditions[:location]).order(created_at: :desc).page(params[:page])
+      @projects = Project.where("category_id in (?) AND cause_id in (?) AND location_id in (?) AND status like 'active'", filter_conditions[:category], filter_conditions[:cause], filter_conditions[:location]).order(created_at: :desc).page(params[:page])
     else 
       if current_user #this section basically identifies if any of the filter is empty from the user profile, and if empty take ALL the values
         @user_categories = current_user.categories.inject([]) { |array,category| array.push(category.id) }.blank? ? Category.all.map {|category| category.id} : current_user.categories.inject([]) { |array,category| array.push(category.id) } 
         @user_causes = current_user.causes.inject([]) { |array,cause| array.push(cause.id) }.blank? ? Cause.all.map {|cause| cause.id} : current_user.causes.inject([]) { |array,cause| array.push(cause.id) }
         @user_locations = current_user.locations.inject([]) { |array,location| array.push(location.id) }.blank? ? Location.all.map {|location| location.id} : current_user.locations.inject([]) { |array,location| array.push(location.id) }
-        @projects = Project.where("category_id in (?) AND cause_id in (?) AND location_id in (?)", @user_categories, @user_causes, @user_locations).order(created_at: :desc).page(params[:page])
+        @projects = Project.where("category_id in (?) AND cause_id in (?) AND location_id in (?) AND status like 'active'", @user_categories, @user_causes, @user_locations).order(created_at: :desc).page(params[:page])
      else
-        @projects = Project.all.order(created_at: :desc).page(params[:page])
+        @projects = Project.where("status like 'active'").order(created_at: :desc).page(params[:page])
       end
     end
 
