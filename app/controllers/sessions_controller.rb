89469b89cf0e2d24a138fb2397e0b797b52ce026
Steve Long <steven989@gmail.com>
2014-05-27 15:44:58 -0400

    change redirect to to redirect_back_or_to

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,10 @@
-    class SessionsController < ApplicationController
-
+class SessionsController < ApplicationController
 
     def create
         if login(params[:email], params[:password])
             redirect_back_or_to projects_path, notice: 'Login successful'
         else 
-            redirect_to projects_path+'#showLogin', notice: 'Your email or password is incorrect.'
+            redirect_back_or_to projects_path+'#showLogin', notice: 'Your email or password is incorrect.'
         end 
     end 
 
